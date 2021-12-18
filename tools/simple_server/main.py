30ec36d60877f3a7e805e4713fd806d64b6d155b
tcoppex <tcoppex@protonmail.com>
2021-12-18 11:20:05 +0100

    [Tools] Update script server to accept args (hostname, port, serving directory).

--- tools/simple_server/main.py
+++ tools/simple_server/main.py
@@ -8,6 +8,12 @@
 #       * https://docs.python.org/3/library/socketserver.html 
 #
 
+from os import getcwd, chdir
+import argparse 
+import http.server
+import socketserver
+import webbrowser
+
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 8080
 
@@ -16,11 +22,10 @@ def run_unique_server(addr, port, rh):
       new browser tab to it. """
 
   try:
-    import socketserver
-    import webbrowser
     with socketserver.TCPServer((addr, port), rh) as server:
       url = 'http://%s:%d/' % (addr, port)
-      print("Serving HTTP on %s port %d (%s), exit with Ctrl-C." % (addr, port, url))
+      print('Serving HTTP on %s port %d (%s), exit with Ctrl-C.' % (addr, port, url))
+      print('Root is "%s"' % getcwd())
       webbrowser.open(url)
       server.serve_forever()
   except OSError:
@@ -30,8 +35,6 @@ def run_unique_server(addr, port, rh):
 
 def run_server(addr = DEFAULT_HOST, port = DEFAULT_PORT):
   """ Configure a server for http request and launch it """
-
-  import http.server
 
   rh = http.server.SimpleHTTPRequestHandler
   rh.extensions_map = {
@@ -48,4 +51,22 @@ def run_server(addr = DEFAULT_HOST, port = DEFAULT_PORT):
   run_unique_server(addr, port, rh)
 
 if __name__ == '__main__':
-  run_server()
+
+  cwd = getcwd()
+  parser = argparse.ArgumentParser(description='Simple HTTP server for wasm mime-type.')
+  parser.add_argument('--serve', 
+                      type=str, 
+                      help=f'Serving directory (default to current).', 
+                      default=cwd)
+  parser.add_argument('--host', 
+                      type=str, 
+                      help=f'Host address (default to "{DEFAULT_HOST}").', 
+                      default=DEFAULT_HOST)
+  parser.add_argument('--port', 
+                      type=int, 
+                      help=f'Listening port (default to {DEFAULT_PORT}).', 
+                      default=DEFAULT_PORT)
+  args = parser.parse_args()
+  chdir(args.serve)
+
+  run_server(args.host, args.port)
