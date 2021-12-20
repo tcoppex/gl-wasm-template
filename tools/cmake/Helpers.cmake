5c9a19097072b045a0f4c3fb05dd1b95b303155e
tcoppex <tcoppex@protonmail.com>
2021-12-20 10:11:57 +0100

    [Cmake] fixed typo.

--- tools/cmake/Helpers.cmake
+++ tools/cmake/Helpers.cmake
@@ -61,7 +61,7 @@ function(helpers_setDefaultBuildType DEFAULT_BUILD_TYPE)
     #set(DEFAULT_BUILD_TYPE "Debug")
   endif()
   if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-    message(STATUS "Setting build type to '${DEFAULT_BUILD_TYPE}' as none was specified.")
+    message(STATUS "Setting build type to '${DEFAULT_BUILD_TYPE}' as none were specified.")
     set(CMAKE_BUILD_TYPE "${DEFAULT_BUILD_TYPE}" CACHE
         STRING "Choose the type of build." FORCE)
     # Set the possible values of build type for cmake-gui
